dfef1679eb2fe6903b6c1b97fae7df2b9771ce10
goreleaserbot <goreleaser@carlosbecker.com>
2023-02-16 15:17:40 -0500

    Brew formula update for ecs-deploy version v0.7.3

--- Formula/ecs-deploy.rb
+++ Formula/ecs-deploy.rb
@@ -5,21 +5,21 @@
 class EcsDeploy < Formula
   desc ""
   homepage "https://github.com/justmiles/ecs-deploy"
-  version "0.7.2"
+  version "0.7.3"
   license "Mozilla Public License Version 2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.2/ecs-deploy_0.7.2_Darwin_arm64.tar.gz"
-      sha256 "7a9d206c93a8757e9138803a1247135c316defcfce96fc613a137be890f7a668"
+      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.3/ecs-deploy_0.7.3_Darwin_arm64.tar.gz"
+      sha256 "2c1158b4ece0148faf6289581e9b0beb61262760e2a20dcc33976f3ce2ee890a"
 
       def install
         bin.install "ecs-deploy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.2/ecs-deploy_0.7.2_Darwin_x86_64.tar.gz"
-      sha256 "fef366ab0beb867204f737dbb80b639f755b4ba9e14274db0d326d2b616355e7"
+      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.3/ecs-deploy_0.7.3_Darwin_x86_64.tar.gz"
+      sha256 "934057b6710da4f8ac956b62dd273c6615c4abac5be67340b8057b0e6d0c5e56"
 
       def install
         bin.install "ecs-deploy"
@@ -29,16 +29,16 @@ class EcsDeploy < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.2/ecs-deploy_0.7.2_Linux_arm64.tar.gz"
-      sha256 "97727f48b2c4ec7e9f1281d6b37a9527f4794e50298902ce997b0fd8253afe31"
+      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.3/ecs-deploy_0.7.3_Linux_arm64.tar.gz"
+      sha256 "4b639c6293557c08dcc43e7182324e5dceb16d047d2ce126eb9657ad020fb90d"
 
       def install
         bin.install "ecs-deploy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.2/ecs-deploy_0.7.2_Linux_x86_64.tar.gz"
-      sha256 "d34f9bad55adb31ebdd1b5469d7a200f3b64f1b28a8175feb1bb87944e24a798"
+      url "https://github.com/justmiles/ecs-deploy/releases/download/v0.7.3/ecs-deploy_0.7.3_Linux_x86_64.tar.gz"
+      sha256 "26d559bb2b54f44632380ffb0a14dbdff3b292cb88da7faf2d4439964d685d95"
 
       def install
         bin.install "ecs-deploy"
